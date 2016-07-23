ce97dc525de38fa30ef9d89af355638e5b7ec82e
Paul Wicks <pwicks86@gmail.com>
2016-07-23 13:16:30 -0700

    Initial commit

---
+++ create-symlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Symlink the configuration files into their appropriate homes
+ln -s ~/.spf13-vim-local/.vimrc.local ~/.vimrc.local
+ln -s ~/.spf13-vim-local/.vimrc.bundles.local ~/.vimrc.bundles.local
+ln -s ~/.spf13-vim-local/.vimrc.before.local ~/.vimrc.before.local

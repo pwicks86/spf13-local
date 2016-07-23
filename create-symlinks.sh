e0b41b295489cbcda061a544f5a05b33ddf3708b
Paul Wicks <pwicks86@gmail.com>
2016-07-23 13:19:45 -0700

    Fix directories

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Symlink the configuration files into their appropriate homes
-ln -s ~/.spf13-vim-local/.vimrc.local ~/.vimrc.local
-ln -s ~/.spf13-vim-local/.vimrc.bundles.local ~/.vimrc.bundles.local
-ln -s ~/.spf13-vim-local/.vimrc.before.local ~/.vimrc.before.local
+ln -s ~/.spf13-local/.vimrc.local ~/.vimrc.local
+ln -s ~/.spf13-local/.vimrc.bundles.local ~/.vimrc.bundles.local
+ln -s ~/.spf13-local/.vimrc.before.local ~/.vimrc.before.local

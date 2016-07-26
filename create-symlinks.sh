3a9081687fffc45babf75767669f4baea1ef5cc1
Paul Wicks <pwicks86@gmail.com>
2016-07-25 17:46:15 -0700

    update script to remove existing files first

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Remove old files
+rm ~/.vimrc.local
+rm ~/.vimrc.bundles.local
+rm ~/.vimrc.before.local
+
 # Symlink the configuration files into their appropriate homes
 ln -s ~/.spf13-local/.vimrc.local ~/.vimrc.local
 ln -s ~/.spf13-local/.vimrc.bundles.local ~/.vimrc.bundles.local
